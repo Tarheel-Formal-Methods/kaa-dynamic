Fixed bug where matrices were not initialized to zero for linear app. computation. Fixed another bug where LinStrat/PCAStrat were not updating their directions in the bundle correctly.

kaa/plotutil.py
@@ -112,7 +112,7 @@ class Plot:
     @params x: index of variable to be plotted as x-axis of desired phase
             y: index of variable to be plotted as y-axis of desired phase
     """
-    def plot2DPhase(self, x, y, separate, lims=None, volume_chart=False, plot_samp_pts=True):
+    def plot2DPhase(self, x, y, separate, lims=None, volume_chart=False):
         assert len(self.flowpipes) != 0, "Plot Object must have at least one flowpipe to plot for 2DPhase."
 
         Timer.start('Phase')
@@ -200,7 +200,7 @@ class Plot:
                 y_coord = y_coord[:num_steps]
 
             ax.plot(x_coord, y_coord, color="k", linewidth=1)
-            ax.scatter(x_coord, y_coord, color=f"C{traj_idx}", s=0.5)
+            ax.scatter(x_coord, y_coord, color=f"C{traj_idx}", s=1)
 
 
     def support_plot(self, flowpipe, flow_idx, x, y, ax):
@@ -240,7 +240,7 @@ class Plot:
                 'Temp patch. Revise to start using LinearSystems for future work.'
                 self.plot_halfspace(x, y, ax, bund.getIntersect(), idx_offset=flow_idx)
             else:
-                for ptope_idx, ptope in enumerate(bund.ptopes):
+                for ptope_idx, ptope in enumerate(bund.all_ptopes):
                     self.plot_halfspace(x, y, ax, ptope, idx_offset=flow_idx+ptope_idx)
 
     """
@@ -252,7 +252,7 @@ class Plot:
         if sys is None: return sys
 
         dim = self.model.dim
-        comple_dim = np.asarray([ True if i in [x,y] else False for i in range(dim) ])
+        comple_dim = np.asarray([True if i in [x,y] else False for i in range(dim)])
 
         vertices = sys.vertices
         proj_vertices = np.unique(vertices[:,comple_dim], axis=0).tolist()
@@ -301,8 +301,21 @@ class Plot:
 class CombinedPlot:
 
     def __init__(self):
-        intersectPlot = None
-        sampledTrajPlot = None
+        self.intersectPlot = None
+        self.sampledTrajPlot = None
+
+class TrajLine:
+
+    def __init__(self, line, start_point, end_point):
+        self.line = line
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def remove(self):
+        self.line.remove()
+        self.start_point.remove()
+        self.end_point.remove()
+
 
 class SlideCompareAnimation(Plot):
 
@@ -430,9 +443,17 @@ class SlideCompareAnimation(Plot):
         'Plot sampled trajectory lines'
         line_obj_list = []
         for init_pt, img_pt in zip(init_pts, img_pts):
-            traj_line = np.asarray([init_pt, img_pt])
-            line, = ax.plot(traj_line[:,x], traj_line[:,y], c="r") #Plot lines.
-            line_obj_list.append(line)
+            traj_line_pts = np.asarray([init_pt, img_pt])
+
+            line, = ax.plot(traj_line_pts[:,x], traj_line_pts[:,y], c="r") #Plot lines.
+            start_pt = ax.scatter(traj_line_pts[0,x], traj_line_pts[0,y])
+            end_pt = ax.scatter(traj_line_pts[1,x], traj_line_pts[1,y], marker='x', s=1.3)
+
+            traj_line_obj = TrajLine(line,
+                                     start_pt,
+                                     end_pt)
+
+            line_obj_list.append(traj_line_obj)
 
         return line_obj_list
 

kaa/temp/lin_app_strat.py
@@ -35,15 +35,22 @@ class AbstractLinStrat(TempStrategy):
     def generate_lin_dir(self, bund, step_num):
 
         if self.dirs is None:
+            print(f"Ran points gen. {self.num_trajs}")
+            print(f"STORED MAT: {self.unit_dir_mat}")
             inv_A = self.__approx_inv_A(bund) #Approx inverse linear transform.
-            lin_dir_mat = np.dot(self.unit_dir_mat, inv_A)
-            
+            lin_dir_mat = np.matmul(self.unit_dir_mat, inv_A)
+
+
             cond_num = np.linalg.cond(lin_dir_mat)
 
             if cond_num > self.cond_threshold:
+                print("Using Normalization method.")
                 norm_lin_dir = normalize_mat(lin_dir_mat)
                 closest_dirs = find_closest_dirs(norm_lin_dir)
                 lin_dir_mat = merge_closest_dirs(norm_lin_dir, closest_dirs, self.dim)
+
+
+            #print(f"Cal Lin Directions Matrix: {lin_dir_mat}")
         else:
             lin_dir_mat = self.dirs.get_dirs_at_step(step_num)
 
@@ -64,7 +71,10 @@ class AbstractLinStrat(TempStrategy):
 
         try:
             approx_A = approx_lin_trans(start_end_tup, self.dim)
+            #print(f"LEAST SQ Matrix Output: {approx_A}")
+
             inv_A = np.linalg.inv(approx_A)
+            #print(f"INV Matrix Output: {inv_A}")
             
         except np.linalg.LinAlgError:
             Output.warning("USING LEAST SQ INVERSE DUE TO SINGULAR VALUE ERROR")
@@ -82,7 +92,7 @@ class LinStrat(AbstractLinStrat):
         num_trajs = 2*model.dim if num_trajs < 0 else num_trajs
         super().__init__(model, num_trajs, cond_threshold)
         self.iter_steps = iter_steps
-        self.last_ptope = None
+        self.ptope_queue = []
 
     """
     Opening LinApp routine
@@ -93,14 +103,14 @@ class LinStrat(AbstractLinStrat):
 
             'Add the components to the bundle and save last generated ptope data.'
             ptope_label = self.add_ptope_to_bund(bund, lin_dir, lin_dir_labels)
-            self.last_ptope = ptope_label
             self.unit_dir_mat = lin_dir
+            self.ptope_queue.append(ptope_label)
 
         return bund
 
     def close_strat(self, bund, step_num):
         if not step_num % self.iter_steps and step_num > 0:
-                self.rm_ptope_from_bund(bund, self.last_ptope)
+                self.rm_ptope_from_bund(bund, self.ptope_queue.pop(0))
 
     """
     Reset the strategy for a new round of computation.
@@ -303,11 +313,10 @@ Normalizes the row of input matrix
 @returns normalized matrix.
 """
 def normalize_mat(mat):
-    
     return mat / np.linalg.norm(mat, ord=2, axis=1, keepdims=True)
 
 def initialize_unit_mat(dim):
-    mat = np.empty((dim, dim))
+    mat = np.zeros((dim, dim))
     for i in range(dim):
         mat[i][i] = 1
     return mat