61afded2b076d199ec5c3fc49503b4438268926e
Edward Kim <edbeta12@gmail.com>
2021-04-12 04:30:46 -0400

    Added mjssing covid, buckcol tests

---
+++ tests/models/test_buckcol.py
@@ -0,0 +1,66 @@
+from models.buckcol import BuckCol_UnitBox
+from kaa.temp.pca_strat import *
+from kaa.temp.lin_app_strat import *
+from kaa.temp.random_static_strat import *
+from kaa.templates import MultiStrategy
+from kaa.experiment import *
+from kaa.experi_init import *
+
+from kaa.settings import PlotSettings, KaaSettings
+from kaa.timer import Timer
+
+def test_box_BuckCol():
+    num_steps = 4
+
+    model = BuckCol_UnitBox()
+
+    experi_input = dict(model=model,
+                        strat=None,
+                        label=f"BuckCol Box Reachable Set",
+                        num_steps=num_steps)
+
+    experi = PhasePlotExperiment(experi_input)
+    experi.execute(0, 1, plot_border_traj=True)
+    Timer.generate_stats()
+
+def test_sliding_equal_BuckCol():
+    num_steps = 60
+    model =  BuckCol_UnitBox()
+    test_equal_sliding_strat(model, num_steps)
+
+def test_max_sliding_lin_strat_BuckCol():
+    num_steps = 20
+    model =  BuckCol_UnitBox()
+    test_max_sliding_lin_strat(model, num_steps)
+
+def test_pca_dominant_BuckCol():
+    num_steps = 20
+    model = BuckCol_UnitBox()
+
+    use_supp = True
+    use_pregen = False
+
+    num_trajs = 5000
+
+    pca_strat = SlidingPCAStrat(model, lifespan=5)
+    lin_strat = SlidingLinStrat(model, lifespan=15)
+
+    experi_input = dict(model=model,
+                        strat=MultiStrategy(pca_strat, lin_strat),
+                        label=f"SlidingPCA Size 15, SlidingLin Size 5",
+                        supp_mode = use_supp,
+                        pregen_mode = use_pregen,
+                        num_trajs=num_trajs,
+                        num_steps=num_steps-1,
+                        max_steps=num_steps)
+
+    if use_supp:
+        file_identifier = "(SUPP)"
+    elif use_pregen:
+        file_identifier = f"(PREGEN: {num_trajs})"
+    else:
+        file_identifier = "(RAND)"
+
+    experi = PhasePlotExperiment(experi_input)
+    experi.execute(0, 1, plot_border_traj=True)
+    Timer.generate_stats()
---
+++ tests/models/test_covid.py
@@ -0,0 +1,106 @@
+from kaa.reach import ReachSet
+from kaa.plotutil import Plot
+from models.covid import Covid, Covid_UnitBox
+from kaa.temp.pca_strat import *
+from kaa.temp.lin_app_strat import *
+from kaa.templates import MultiStrategy
+
+from kaa.timer import Timer
+from kaa.trajectory import Traj
+from kaa.experi_init import *
+
+from kaa.bundle import BundleMode
+
+def test_sapo_Covid():
+    num_steps = 150
+    model = Covid(delta=0.5)
+
+    experi_input = dict(model=model, #Encompass strat initilizations?
+                        strat=None,
+                        label="SapoCovid",
+                        supp_mode = False,
+                        pregen_mode = False,
+                        num_trajs=5000,
+                        num_steps=num_steps-1,
+                        max_steps=num_steps)
+
+    harosc = ProjectionPlotExperiment(experi_input)
+    harosc.execute(0,1,2)
+    Timer.generate_stats()
+
+def test_sapo_vol_Covid():
+    use_supp = True
+    use_pregen = False
+
+    num_trajs = 5000
+    num_steps = 150
+
+    model = Covid(delta=0.5)
+    experi_input = dict(model=model, #Encompass strat initilizations?
+                        strat=None,
+                        label="SapoCovid",
+                        supp_mode = use_supp,
+                        pregen_mode = use_pregen,
+                        num_trajs=num_trajs,
+                        num_steps=num_steps-1,
+                        max_steps=num_steps)
+
+    harosc = VolumeExperiment(experi_input)
+    harosc.execute(1)
+
+def test_sapo_skewed_compare_Covid():
+    num_steps = 100
+    use_supp = True
+    use_pregen = False
+
+    num_trajs = 5000
+    num_steps = 70
+
+    pca_window_size = 10
+    lin_window_size = 10
+    model_1 = Covid(delta=0.5)
+
+    experi_input_1 = dict(model=model_1, #Encompass strat initilizations?
+                        strat=None,
+                        label="SapoCovid",
+                        num_steps=num_steps-1,
+                        supp_mode = use_supp,
+                        pregen_mode = use_pregen,
+                        num_trajs=num_trajs,
+                        max_steps=num_steps)
+
+
+
+    model = Covid_UnitBox(delta=0.5)
+
+    pca_strat = SlidingPCAStrat(model, lifespan=pca_window_size)
+    lin_strat = SlidingLinStrat(model, lifespan=lin_window_size)
+
+    experi_input_2 = dict(model=model,
+                        strat=MultiStrategy(pca_strat, lin_strat),
+                        label=f"SlidingPCA Step {pca_window_size} and SlidingLin Step {lin_window_size}",
+                        supp_mode = use_supp,
+                        pregen_mode = use_pregen,
+                        num_trajs=num_trajs,
+                        num_steps=num_steps-1,
+                        max_steps=num_steps)
+
+    harosc = Experiment(experi_input_1, experi_input_2, label=f"SAPO1010COMP")
+    harosc.execute(1)
+
+def test_equal_sliding_strat_Covid():
+    model = Covid_UnitBox()
+    test_equal_sliding_strat(model, 100, vars=(3,4,6))
+
+def test_ran_strat_Covid():
+    model = Covid_UnitBox()
+    test_ran_strat(model, 100, 5000, use_supp = True, use_pregen = False)
+
+def test_skewed_sliding_strat_comb_Covid():
+    unit_model = Covid_UnitBox()
+    model = Covid()
+    test_skewed_sliding_strat_comb(unit_model, 40, 5000, use_supp=True, use_pregen=False, use_sapo=model)
+
+def test_sliding_strat_comb_Covid():
+    model = Covid_UnitBox()
+    test_sliding_strat_comb(model, 150, 5000, use_supp=True, use_pregen=False)
