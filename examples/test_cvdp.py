1ee80da0918d22caaa3dfcdfb5e3d9d087ac53c1
Edward Kim <edbeta12@gmail.com>
2021-06-29 17:11:23 -0400

    Repo initialized for ARCH benchmarks

--- examples/test_cvdp.py
+++ examples/test_cvdp.py
@@ -27,7 +27,7 @@ def test_arch_CVDP():
     delta_one = 0.1
     delta_two = 0.1
 
-    num_steps_one = int(8 / delta_one) - 1
+    num_steps_one = int(7 / delta_one) - 1
     num_steps_two = int(8 / delta_two) - 1
     init_box_one = ((1.25, 1.55), (2.35, 2.45), (1.25, 1.55), (2.35, 2.45))
     init_box_two = ((1.55, 1.85), (2.35, 2.45), (1.55, 1.85), (2.35, 2.45))
@@ -38,37 +38,32 @@ def test_arch_CVDP():
     #pca_window_size = 10
     #lin_window_size = 0
 
-    #pca_strat_one = SlidingPCAStrat(model_one, lifespan=pca_window_size)
-    #lin_strat_one = SlidingLinStrat(model_one, lifespan=lin_window_size)
-
     ran_static_strat_one = RandomDiagStaticStrat(model_one, 30)
     #pca_strat_one = SlidingPCAStrat(model_one, 10)
 
     experi_input_one = dict(model=model_one,
-                        strat=ran_static_strat_one,
-                        label=f"CVDP mu=1",
-                        num_steps=num_steps_one,
-                        supp_mode = use_supp,
-                        pregen_mode = use_pregen,
-                        num_trajs=num_trajs,
-                        trans_mode=BundleTransMode.AFO)
-
-    #pca_strat_two = SlidingPCAStrat(model_two, lifespan=pca_window_size)
-    #lin_strat_two = SlidingLinStrat(model_two, lifespan=lin_window_size)
+                            strat=ran_static_strat_one,
+                            label=f"CVDP mu=1",
+                            num_steps=num_steps_one,
+                            supp_mode = use_supp,
+                            pregen_mode = use_pregen,
+                            num_trajs=num_trajs,
+                            trans_mode=BundleTransMode.AFO,
+                            restrict_inter=(-5,5))
 
     ran_static_strat_two = RandomDiagStaticStrat(model_two, 30)
     #pca_strat_two = SlidingPCAStrat(model_two, 10)
 
 
     experi_input_two = dict(model=model_two,
-                        strat=ran_static_strat_two,
-                        label=f"CVDP mu=2",
-                        num_steps=num_steps_two,
-                        supp_mode = use_supp,
-                        pregen_mode = use_pregen,
-                        num_trajs=num_trajs,
-                        trans_mode=BundleTransMode.AFO)
-
+                            strat=ran_static_strat_two,
+                            label=f"CVDP mu=2",
+                            num_steps=num_steps_two,
+                            supp_mode = use_supp,
+                            pregen_mode = use_pregen,
+                            num_trajs=num_trajs,
+                            trans_mode=BundleTransMode.AFO,
+                            restrict_inter=(-5,5))
 
     experi = PhasePlotExperiment(experi_input_one, experi_input_two, label="CVDP20")
     experi_data = experi.execute(0, 1)
