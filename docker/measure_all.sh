31eb40d99e46d429a80b2f93e12fcb4798b1e5ab
Edward Kim <edbeta12@gmail.com>
2021-07-05 03:22:06 -0400

    Commiting dockerimage and benchmark batch script into master

---
+++ docker/measure_all.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+rm -Rf results
+mkdir results
+sudo docker build -t kaa-arch .
+sudo docker run -v $PWD/results:/kaa-dynamic/figures -v $PWD/results:/kaa-dynamic/data kaa-arch:latest \
+      python3 -c "from examples.test_arch21 import *; run_all_benchmarks()"
